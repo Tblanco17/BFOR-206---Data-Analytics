c6cc10ac75f189674b05eef3b7d60d8b6ea22976
Tyler Blanco <42285917+Tblanco17@users.noreply.github.com>
2019-03-01 10:17:12 -0500

    Add files via upload
    First Powershell lab that outputs all files that were recently written to and are over 1mb in size.

---
+++ Powershell1.ps1
@@ -0,0 +1,8 @@
+﻿
+$recentfiles=(ls . -File -Recurse | where {$_.LastWriteTime -ge (get-date).AddHours(-2)})
+
+foreach ($file in $recentfiles) {
+
+if ($file.Length -ge 1mb) { 
+Write-host $file.Name is $file.Length bytes 
+}}
